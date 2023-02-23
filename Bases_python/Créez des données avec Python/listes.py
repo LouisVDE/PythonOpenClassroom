4b49e2be2f67c4f0715028cc3b87f03b034a2694
LouisVDE <louis.vanden.eede@icloud.com>
2023-02-23 21:59:30 +0100

    Premiers exercices en python

---
+++ Bases_python/Créez des données avec Python/listes.py
@@ -0,0 +1,12 @@
+#Déclaration d'une liste
+plateformes_sociales = ["Facebook", "Instagram", "Twitter", "Snapchat"]
+print(plateformes_sociales[0])
+print(plateformes_sociales[1])
+print(plateformes_sociales[2])
+print(plateformes_sociales[3])
+#Ajoute un élément a la liste
+plateformes_sociales.append("TikTok")
+print(plateformes_sociales[4])
+#Supprime un élément de la liste
+plateformes_sociales.remove("TikTok")
+print(plateformes_sociales)
---
+++ Bases_python/Créez des données avec Python/variables.py
@@ -0,0 +1,9 @@
+#String
+livre = "De la Terre a la Lune - Jules Verne"
+print(livre)
+#int,float
+annee_de_sortie = 1865
+print(annee_de_sortie)
+#Bool
+livre_qualitatif = True
+print("Livre qualitatif ? :", livre_qualitatif)
